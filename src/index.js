d1c71462d08bf1845e68182035b0cbba8d80c2a4
Emery Tate <tateejt@gmail.com>
2020-02-03 21:42:39 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+    const formEl = document.querySelector('#create-task-form')
+    const unorderedList = document.querySelector("#tasks");
+
+  formEl.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const newTask = document.querySelector('#new-task-description')
+    const task = new Task(newTask.value)
+    console.log(task)
+
+    unorderedList.append(task.element)
+    newTask.value = '';
+  })
+
+
+  unorderedList.addEventListener('click', (e)=>{
+    if(e.target.className === 'remove-btn') {
+      e.target.parentNode.remove()
+    }
+  })
+})
+
+class Task {
+  constructor(value){
+    this.element = this.createTaskElement(value)
+  }
+
+  createTaskElement(value) {
+    const newTaskEl = document.createElement("li");
+    newTaskEl.innerText = value;
+    const removeButton = document.createElement("button");
+    removeButton.innerText = 'Remove';
+    removeButton.className= 'remove-btn';
+    newTaskEl.append(removeButton)
+    return newTaskEl
+  }
+}
