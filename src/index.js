08bed58d6ddb3c960cc03ac2765e3ed91cdd7699
Emery Tate <tateejt@gmail.com>
2020-02-03 21:37:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
   const taskList = new TaskList();
-  
+
   const listContainer = document.getElementById("list");
   const renderApp = () => (listContainer.innerHTML = taskList.render());
-  
+
+  // Add Form Behavior Here!
+
   renderApp()
 });
